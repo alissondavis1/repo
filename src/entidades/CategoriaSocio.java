272a6ed475f6865451ed9d5dfeaf278e19aee42a
Alisson <alissondavis1@hotmail.com>
2013-03-22 16:10:47 -0300

    Revert "arquivos apagador  - pacote teste  e atribulto teste"
    This reverts commit 1860191e14308223eb9489c2b27655518301c247.

--- src/entidades/CategoriaSocio.java
+++ src/entidades/CategoriaSocio.java
@@ -36,11 +36,19 @@ import javax.xml.bind.annotation.XmlTransient;
 public class CategoriaSocio implements Serializable {
     private static final long serialVersionUID = 1L;
     
+    
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "Id")
-    private Integer id;   
+    private Integer id;
+    
+    
+    private String teste;
+    
+    
+    
     @Basic(optional = false)
     @Column(name = "Nome")
     private String nome;
