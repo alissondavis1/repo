aa4837024d8ace05899dd7e8142fa8672569e078
aliss_000 <aliss_000@Alisson>
2013-03-31 15:18:52 -0300

    implementação do Splash

---
+++ src/telas/Splash.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telas;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.JWindow;
+import javax.swing.UnsupportedLookAndFeelException;
+import org.netbeans.lib.awtextra.AbsoluteConstraints;
+import org.netbeans.lib.awtextra.AbsoluteLayout;
+
+
+public class Splash extends JWindow{
+    
+     AbsoluteConstraints absBarra, absImage, absTexto;
+     AbsoluteLayout absl;
+     ImageIcon image;
+     JLabel label, texto;
+     JProgressBar progress;
+    public Splash(){
+        
+        absBarra = new AbsoluteConstraints(0,290);
+        absImage = new AbsoluteConstraints(0,0);
+        absTexto = new AbsoluteConstraints(0,250);
+        absl = new AbsoluteLayout();
+        label = new JLabel();
+        image = new ImageIcon("img/automacao.png");
+        label.setIcon(image);
+        progress = new JProgressBar();
+        progress.setPreferredSize(new Dimension(300,20));
+        progress.setBackground(Color.orange);
+        progress.setString("Carregando");
+        progress.setStringPainted(true);
+        setLayout(absl);
+        getContentPane().add(label,absImage);
+        getContentPane().add(progress,absBarra);
+        new Thread(){
+            
+            public void run(){
+                
+                int i = 0;
+                while(i < 101){
+                    
+                    progress.setValue(i);
+                    i++;
+                    try {    
+                        sleep(30);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Splash.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                new TelaPrincipal().setVisible(true);
+                dispose();
+               
+            }
+            
+            
+        }.start();
+        setLocation(((Toolkit.getDefaultToolkit().getScreenSize().width  / 3) - (this.getWidth() / 3)),   
+        ((Toolkit.getDefaultToolkit().getScreenSize().height / 3) - (this.getHeight() / 3)));  
+        pack();
+        setVisible(true);
+        
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        try {
+            Properties p = new Properties();
+            p.put("logoString", "ACAL2000");
+            com.jtattoo.plaf.mcwin.McWinLookAndFeel.setTheme(p);
+           
+            javax.swing.UIManager.setLookAndFeel(new com.jtattoo.plaf.mcwin.McWinLookAndFeel());
+            /* Create and display the form */
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(TelaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        new Splash();
+        
+        
+        
+    }
+    
+}
